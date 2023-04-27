bad42e153f369cfa323c4822c2da9198b5dcbbb2
Pheithar <avalverdemahou@gmail.com>
2023-04-27 11:13:51 +0200

    Add leg rigging

--- scripts/CreateAntBot.py
+++ scripts/CreateAntBot.py
@@ -69,8 +69,8 @@ bpy.ops.object.mode_set(mode="EDIT")
 
 #   Add 1 bone for head
 headBone = armature.edit_bones.new("HeadBone")
-HEAD_OFFSET = -0.2
-HEAD_SIZE = 0.59
+HEAD_OFFSET = -0.16
+HEAD_SIZE = 0.55
 HEAD_START = HEAD_SIZE/2 + HEAD_OFFSET
 HEAD_END = HEAD_START - HEAD_SIZE
 
@@ -80,7 +80,7 @@ headBone.tail = (0, HEAD_END, 0)
 #   Add 1 bone for body
 bodyBone = armature.edit_bones.new("BodyBone")
 BODY_OFFSET = 0
-BODY_SIZE = 0.71
+BODY_SIZE = 0.76
 BODY_START = HEAD_END + BODY_OFFSET
 BODY_END = BODY_START - BODY_SIZE * segmentNumber
 
@@ -88,6 +88,38 @@ bodyBone.head = (0, BODY_START, 0)
 bodyBone.tail = (0, BODY_END, 0)
 
 bodyBone.parent = headBone
+
+#   Add 2 bones for each leg
+LEG_OFFSET = 0.764
+LEG_X_OFFSET = 0.94
+LEG_SIZE = 0.367 * legLength / 2
+
+LEG_START = HEAD_END - LEG_OFFSET / 2
+LEG_END = LEG_START - LEG_SIZE
+for i in range(segmentNumber):
+    legBoneAR = armature.edit_bones.new(f"LegBoneAnteriorRight_{i}")
+    legBoneAR.head = (LEG_X_OFFSET, LEG_START - i * LEG_OFFSET, 0)
+    legBoneAR.tail = (LEG_X_OFFSET + LEG_SIZE, LEG_START - i * LEG_OFFSET, 0)
+    legBoneAR.parent = bodyBone
+
+    legBonePR = armature.edit_bones.new(f"LegBonePosteriorRight_{i}")
+    legBonePR.head = (LEG_X_OFFSET + LEG_SIZE, LEG_START - i * LEG_OFFSET, 0)
+    legBonePR.tail = (LEG_X_OFFSET + 2 * LEG_SIZE, LEG_START - i * LEG_OFFSET, 0)
+    legBonePR.parent = legBoneAR
+
+    legBoneAL = armature.edit_bones.new(f"LegBoneAnteriorLeft_{i}")
+    legBoneAL.head = (-LEG_X_OFFSET, LEG_START - i * LEG_OFFSET, 0)
+    legBoneAL.tail = (-LEG_X_OFFSET - LEG_SIZE, LEG_START - i * LEG_OFFSET, 0)
+    legBoneAL.parent = bodyBone
+    
+    legBonePL = armature.edit_bones.new(f"LegBonePosteriorLeft_{i}")
+    legBonePL.head = (-LEG_X_OFFSET - LEG_SIZE, LEG_START - i * LEG_OFFSET, 0)
+    legBonePL.tail = (-LEG_X_OFFSET - 2 * LEG_SIZE, LEG_START - i * LEG_OFFSET, 0)
+    legBonePL.parent = legBoneAL
+
+
+
+
 
 
 bpy.ops.object.mode_set(mode="OBJECT")
